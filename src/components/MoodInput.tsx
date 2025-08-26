be406e6cc8fa5b53c7239f9b2c9d8b0b7158a646
sowndharajan-s <sowndharrajanmanokaran@gmail.com>
2025-08-26 23:54:20 +0530

    moodmail

---
+++ src/components/MoodInput.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Input } from './ui/input'
+import { Button } from './ui/button'
+
+type Props = {
+    mood:string,
+    setMood:(val:string) => void,
+    onGenerate:() => void,
+    disabled:boolean
+}
+
+
+const MoodInput = ({mood,setMood,onGenerate,disabled}:Props) => {
+  return (
+    <div className=' space-y-4'>
+        <Input placeholder='How are feeling today ? (happy,sad,angry ,....)' value={mood} onChange={(e) => setMood(e.target.value)} disabled={disabled}
+        />
+        <Button className=' w-full' onClick={onGenerate} disabled={disabled}>
+            Generate email template
+        </Button>
+        
+        
+    </div>
+  )
+}
+
+export default MoodInput
---
+++ src/pages/Home.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import MoodInput from '@/components/MoodInput'
+import MoodOutput from '@/components/MoodOutput'
+
+const Home = () => {
+    const [mood,setMood] = useState('')
+    const [subject,setSubject] = useState('')
+    const [footer,setFooter] = useState('')
+    const [generator, setGenerator] = useState(false)
+
+    const handleGenerate = () => {
+        const lowermood = mood.toLowerCase();
+
+        if(lowermood.includes("happy")){
+            setSubject("Felling great");
+            setFooter("stay awesome");
+        }else if(lowermood.includes("sad")){
+            setSubject("just another taugh")
+            setFooter("sending hugs")
+        }else if(lowermood.includes("angry")){
+            setSubject("need to cool")
+            setFooter("deep breaths")
+        }else{
+            setSubject("Mood Update")
+            setFooter("catch you later")
+        }
+
+        setGenerator(true)
+
+        
+        
+    }
+
+    const handleReset = () => {
+       setMood("");
+       setSubject("");
+       setFooter("");
+       setGenerator(false)
+    }
+
+  return (
+    <div className=' max-w-xl mx-auto shadow-xl p-6 mt-20 rounded-lg bg-white space-y-6'>
+        <h2 className=' font-bold text-2xl text-gray-800'>MOODMAIL GENERATOR</h2>
+        <MoodInput mood={mood} setMood={setMood} onGenerate={handleGenerate} disabled={generator}/>
+
+        <MoodOutput subject={subject} footer={footer} onReset={handleReset}
+        />
+
+    </div>
+  )
+}
+
+export default Home
